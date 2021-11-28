6bf8a78ecf05d372cf5fc0d44c795590ac55365a
The Brahmnic Boy <thebrahmnicboy@gmail.com>
2021-11-28 20:30:32 +0530

    Adjust Navbar for mobile

---
+++ tools.js
@@ -0,0 +1,34 @@
+const navTitle = document.getElementById('brand-con');
+const navOptionCon = document.getElementById('nav-link-con');
+const navButton = document.getElementById('nav-btn');
+const navOptions = document.querySelector('.nav-links');
+
+// Used for making sure 100vw doesn't overflow horizontally
+// Keywords : Horizontal Overflow
+function setVw() {
+  let vw = document.documentElement.clientWidth / 100;
+  document.documentElement.style.setProperty('--vw', `${vw}px`);
+}
+
+setVw();
+window.addEventListener('resize', setVw);
+// Used for making sure 100vw doesn't overflow horizontally
+
+// Navbar toggler
+function isHidden(el) {
+    return (el.offsetParent === null);
+}
+
+function loadNavBar() {
+  if (navTitle.classList.contains('visible')) {
+    navTitle.classList.remove('visible');
+    navOptions.classList.remove('visible');
+    navOptionCon.classList.remove('visible');
+  } else {
+    navTitle.classList.add('visible');
+    navOptions.classList.add('visible');
+    navOptionCon.classList.add('visible');
+  }
+}
+
+navButton.addEventListener("click", loadNavBar);
