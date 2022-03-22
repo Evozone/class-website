90c4f3d6bfbbfdc59bce45b79f40ab1716dcd593
The Brahmnic Boy <thebrahmnicboy@gmail.com>
2022-03-22 22:32:50 +0530

    Add tools and start work on Timeline

--- app.js
+++ app.js
@@ -1,40 +1,12 @@
-//Importing Express Module
-const express = require("express");
-const bodyParser = require('body-parser');
-const https = require('https');
+// Hide navbar on scroll
+var prevScrollpos = window.pageYOffset;
 
-//Importing Database
-const admin = require("firebase-admin");
-const db = require("./db");
-
-//Initialising Express
-const app = express();
-
-// Configuring Express
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-//Creating APIs
-// const url = 'https://ptb.discord.com/api/guilds/804986812425699339/widget.json';
-// https.get(url, (response) => {
-//     console.log(response);
-// })
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
-
-app.get("/tools", (req, res) => {
-    res.sendFile(__dirname + "/tools.html");
-});
-
-app.get("/timeline", (req, res) => {
-    res.sendFile(__dirname + "/timeline.html");
-});
-
-// Detect port number from the Node Server or use 5000
-const PORT = process.env.PORT || 5000;
-
-// Listen for URIs on a port
-app.listen(PORT, () => console.log(`Server started at ${PORT}`));
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav-scroll").classList.remove('nav-hidden');
+    } else {
+        document.getElementById("nav-scroll").classList.add('nav-hidden');
+    }
+    prevScrollpos = currentScrollPos;
+}
